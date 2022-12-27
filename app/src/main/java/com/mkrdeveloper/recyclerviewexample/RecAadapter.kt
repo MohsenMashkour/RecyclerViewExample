9eb2d6d652348a1b04cbd054d09bdef5273417f3
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2022-12-27 13:40:23 +0100

    second commit

--- app/src/main/java/com/mkrdeveloper/recyclerviewexample/RecAadapter.kt
+++ app/src/main/java/com/mkrdeveloper/recyclerviewexample/RecAadapter.kt
@@ -5,15 +5,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class RecAadapter(private val pizzaList : ArrayList<Pizza>) : RecyclerView.Adapter<RecAadapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val imgTitle = itemView.findViewById<ImageView>(R.id.imgTitle)
-        val tvName = itemView.findViewById<TextView>(R.id.tvName)
-        val tvIng = itemView.findViewById<TextView>(R.id.tvIng)
+        val imgTitle: ImageView = itemView.findViewById(R.id.imgTitle)
+        val tvName: TextView = itemView.findViewById(R.id.tvName)
+        val tvIng: TextView = itemView.findViewById(R.id.tvIng)
+        val constraint_row : ConstraintLayout = itemView.findViewById(R.id.constraint_row)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,9 +30,21 @@ class RecAadapter(private val pizzaList : ArrayList<Pizza>) : RecyclerView.Adapt
         holder.imgTitle.setImageResource(currentItem.imageTitle)
         holder.tvName.text = currentItem.name
         holder.tvIng.text = currentItem.ingredients
+
+        val cont = holder.constraint_row.context
+        holder.constraint_row.setOnClickListener {
+            Toast.makeText(cont, "the item ${currentItem.name} is clicked", Toast.LENGTH_SHORT).show()
+        }
+
+
+        holder.constraint_row.setOnLongClickListener(View.OnLongClickListener {
+            Toast.makeText(cont, "the item ${currentItem.name} is long clicked", Toast.LENGTH_SHORT).show()
+
+            return@OnLongClickListener true
+        })
     }
 
     override fun getItemCount(): Int {
-        return  pizzaList.size
+        return pizzaList.size
     }
 }
